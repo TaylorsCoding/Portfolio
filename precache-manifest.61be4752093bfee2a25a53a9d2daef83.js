64e2905567bb8d82e98e6c98ddf89ea7ab45b46f
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-10-18 23:25:11 -0400

    Updates

---
+++ precache-manifest.61be4752093bfee2a25a53a9d2daef83.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86809e31b61887cffc6245b6e50bb819",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "621bd5e997d008d4470d",
+    "url": "/Portfolio/static/css/main.ca3dfb46.chunk.css"
+  },
+  {
+    "revision": "e2c2aef081a3a8638ca9",
+    "url": "/Portfolio/static/js/2.ca57223e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Portfolio/static/js/2.ca57223e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621bd5e997d008d4470d",
+    "url": "/Portfolio/static/js/main.9dd696e7.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "0029ddd18529c86e1ef8f73dd086632f",
+    "url": "/Portfolio/static/media/clouds.0029ddd1.jpg"
+  },
+  {
+    "revision": "c529121a7166bebcc06f0fdf249185ee",
+    "url": "/Portfolio/static/media/evchome.c529121a.png"
+  },
+  {
+    "revision": "669238cfe6a71525214dc51c19dc27b3",
+    "url": "/Portfolio/static/media/evcmap.669238cf.png"
+  },
+  {
+    "revision": "55c156e3d63b572ad5467d786b7931a2",
+    "url": "/Portfolio/static/media/neverlost.55c156e3.png"
+  },
+  {
+    "revision": "25cb5863db824ae3b6b02a2d64722716",
+    "url": "/Portfolio/static/media/profpic.25cb5863.jpeg"
+  },
+  {
+    "revision": "df7d373b9fec17762007aa2dee3bc481",
+    "url": "/Portfolio/static/media/quizAPP.df7d373b.png"
+  }
+]);
